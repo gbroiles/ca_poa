58a92fd8b249e73076ffd95fa338f592eab3e0f8
gbroiles <gbroiles@gmail.com>
2020-03-10 20:07:39 -0700

    more fields added to resource.py, field replace not working?

--- poa.py
+++ poa.py
@@ -1,34 +1,49 @@
 from fpdf import FPDF
 import resource
 
+
 class PDF(FPDF):
     def footer(self):
         # Position at 1.5 cm from bottom
         self.set_y(-15)
         # Arial italic 8
-        self.set_font('Arial', 'I', 8)
+        self.set_font("Arial", "I", 8)
         # Page number
-        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
+        self.cell(0, 10, "Page " + str(self.page_no()) + "/{nb}", 0, 0, "C")
 
-def fill_fields(text, fields)
+
+def fill_fields(text, fields):
     for field, value in fields.items():
+        field = "[{}]".format(field)
         print(field, value)
+        if text.find(field):
+            print("Found {}".format(field))
+        text.replace(field, value)
+    print(text)
+    return text
+
 
 # Instantiation of inherited class
 
-fill_fields(resource.form, resource.testfields)
+form = fill_fields(resource.form, resource.testfields)
+notary_ack = fill_fields(resource.notary_ack, resource.testfields)
 
 pdf = PDF()
 pdf.alias_nb_pages()
 pdf.add_page()
-pdf.set_font('Times', '', 14)
-pdf.multi_cell(0, 6, 'California Uniform Statutory Form Power of Attorney\n(California Probate Code §4401)', border=1, align='C')
-pdf.set_font('Times', '', 10)
-pdf.multi_cell(0, 4, resource.preamble, align='J', border=0)
-pdf.set_font('Times', '', 12)
-pdf.multi_cell(0, 5, resource.form, align='L', border=0)
-pdf.set_font('Times', '', 10)
-pdf.multi_cell(0, 4, resource.notary_box, border=1, align='L')
-pdf.multi_cell(0, 4, resource.notary_ack, border=0, align='L')
-pdf.output('poa_test.pdf', 'F')
-
+pdf.set_font("Times", "", 14)
+pdf.multi_cell(
+    0,
+    6,
+    "California Uniform Statutory Form Power of Attorney\n(California Probate Code §4401)",
+    border=1,
+    align="C",
+)
+pdf.set_font("Times", "", 10)
+pdf.multi_cell(0, 4, resource.preamble, align="J", border=0)
+pdf.set_font("Times", "", 12)
+pdf.multi_cell(0, 5, form, align="L", border=0)
+pdf.set_font("Times", "", 10)
+pdf.multi_cell(0, 4, resource.notary_box, border=1, align="L")
+pdf.multi_cell(0, 4, resource.notary_ack, border=0, align="L")
+pdf.output("poa_test.pdf", "F")
--- resource.py
+++ resource.py
@@ -1,8 +1,22 @@
+testfields = {
+    "principal_name": "Jasper Floppydog",
+    "agent_list": "Emma Dog, Charlie Dog, and Ginger Dog",
+    "empty_line": ("_" * 60),
+    "joint_powers": "SEPARATELY",
+    "signature_line": ("_" * 30),
+    "day_num": "30th",
+    "month": "February",
+    "year": "2020",
+    "notarization_state": "CA",
+    "notarization_county": "Butte",
+    "empty_field": ("_" * 20),
+}
+
 preamble = """
 NOTICE: THE POWERS GRANTED BY THIS DOCUMENT ARE BROAD AND SWEEPING. THEY ARE EXPLAINED IN THE UNIFORM STATUTORY FORM POWER OF ATTORNEY ACT (CALIFORNIA PROBATE CODE SECTIONS 4400-4465). THE POWERS LISTED IN THIS DOCUMENT DO NOT INCLUDE ALL POWERS THAT ARE AVAILABLE UNDER THE PROBATE CODE. ADDITIONAL POWERS AVAILABLE UNDER THE PROBATE CODE MAY BE ADDED BY SPECIFICALLY LISTING THEM UNDER THE SPECIAL INSTRUCTIONS SECTION OF THIS DOCUMENT. IF YOU HAVE ANY QUESTIONS ABOUT THESE POWERS, OBTAIN COMPETENT LEGAL ADVICE. THIS DOCUMENT DOES NOT AUTHORIZE ANYONE TO MAKE MEDICAL AND OTHER HEALTH-CARE DECISIONS FOR YOU. YOU MAY REVOKE THIS POWER OF ATTORNEY IF YOU LATER WISH TO DO SO.
 """
 form = """
-I, {principal_name}, appoint {agent_list} as my agent (attorney-in-fact) to act for me in any lawful way with respect to the following initialed subjects:
+I, [principal_name], appoint [agent_list] as my agent (attorney-in-fact) to act for me in any lawful way with respect to the following initialed subjects:
 
 TO GRANT ALL OF THE FOLLOWING POWERS, INITIAL THE LINE IN FRONT OF (N) AND IGNORE THE LINES IN FRONT OF THE OTHER POWERS.
 TO GRANT ONE OR MORE, BUT FEWER THAN ALL, OF THE FOLLOWING POWERS, INITIAL THE LINE IN FRONT OF EACH POWER YOU ARE GRANTING.
@@ -28,8 +42,8 @@ YOU NEED NOT INITIAL ANY OTHER LINES IF YOU INITIAL LINE (N).
 
 SPECIAL INSTRUCTIONS:
 ON THE FOLLOWING LINES YOU MAY GIVE SPECIAL INSTRUCTIONS LIMITING OR EXTENDING THE POWERS GRANTED TO YOUR AGENT.
-{empty_line}
-{empty_line}
+[empty_line]
+[empty_line]
 
 UNLESS YOU DIRECT OTHERWISE ABOVE, THIS POWER OF ATTORNEY IS EFFECTIVE IMMEDIATELY AND WILL CONTINUE UNTIL IT IS REVOKED.
 
@@ -38,18 +52,18 @@ This power of attorney will continue to be effective even though I become incapa
 STRIKE THE PRECEDING SENTENCE IF YOU DO NOT WANT THIS POWER OF ATTORNEY TO CONTINUE IF YOU BECOME INCAPACITATED.
 
 EXERCISE OF POWER OF ATTORNEY WHERE MORE THAN ONE AGENT DESIGNATED
-If I have designated more than one agent, the agents are to act {joint_powers}.
+If I have designated more than one agent, the agents are to act [joint_powers].
 
 IF YOU APPOINTED MORE THAN ONE AGENT AND YOU WANT EACH AGENT TO BE ABLE TO ACT ALONE WITHOUT THE OTHER AGENT JOINING, WRITE THE WORD 'SEPARATELY' IN THE BLANK SPACE ABOVE. IF YOU DO NOT INSERT ANY WORD IN THE BLANK SPACE, OR IF YOU INSERT THE WORD 'JOINTLY', THEN ALL OF YOUR AGENTS MUST ACT OR SIGN TOGETHER.
 
 I agree that any third party who receives a copy of this document may act under it. A third party may seek identification. Revocation of the power of attorney is not effective as to a third party until the third party has actual knowledge of the revocation. I agree to indemnify the third party for any claims that arise against the third party because of reliance on this power of attorney.
 
-Signed this {day_num} day of {month}, {year}.
+Signed this [day_num] day of [month], [year].
 
 
 ______________________________
-{principal_name}
-State of {notarization_state}, County of {notarization_county} 
+[principal_name]
+State of [notarization_state], County of [notarization_county] 
 
 BY ACCEPTING OR ACTING UNDER THE APPOINTMENT, THE AGENT ASSUMES THE FIDUCIARY AND OTHER LEGAL RESPONSIBILITIES OF AN AGENT.
 
@@ -61,9 +75,9 @@ A notary public or other officer completing this certificate verifies only the i
 
 notary_ack = """
 State of California
-County of {notarization_county}
+County of [notarization_county]
 
-On {empty_field} before me, {empty_field}, personally appeared {principal_name}, who proved to me on the basis of satisfactory evidence to be the person(s) whose name(s) is/are subscribed to the within instrument and acknowledged to me that he/she/they executed the same in his/her/their authorized capacity(ies), and that by his/her/their signature(s) on the instrument the person(s), or the entity upon behalf of which the person(s) acted, executed the instrument.
+On [empty_field] before me, [empty_field], personally appeared [principal_name], who proved to me on the basis of satisfactory evidence to be the person(s) whose name(s) is/are subscribed to the within instrument and acknowledged to me that he/she/they executed the same in his/her/their authorized capacity(ies), and that by his/her/their signature(s) on the instrument the person(s), or the entity upon behalf of which the person(s) acted, executed the instrument.
 
 I certify under PENALTY OF PERJURY under the laws of the State of California that the foregoing paragraph is true and correct.
 WITNESS my hand and official seal:
@@ -71,6 +85,6 @@ WITNESS my hand and official seal:
 
 
 
-{signature_line}
+[signature_line]
 Notary Public
 """
